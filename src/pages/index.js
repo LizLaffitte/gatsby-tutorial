976595ce24964442937c57c4fbf83053e845c468
lizlaffitte <laffitte.digital@gmail.com>
2021-04-11 16:56:00 -0400

    Adding a plugin.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import {Link} from "gatsby"
 
 export default function Home() {
   return (
-    <div style={{color: `purple`}}>
-      <Link to="/contact">Contact</Link>
-      <Header headerText="Liz Laffitte"/>
-      <p>Fullstack Software Engineer</p>
-      <img src="https://source.unsplash.com/random/400x200" />
-    </div>
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
   )
 }
